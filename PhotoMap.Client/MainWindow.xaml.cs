ac218c5e7091e6d6b4b605b1043c9f1734f6906d
grankko <grankko@gmail.com>
2021-01-08 19:30:08 +0100

    scan directory for images (jpg) and sets pushpins preview image on pushpin click

--- PhotoMap.Client/MainWindow.xaml.cs
+++ PhotoMap.Client/MainWindow.xaml.cs
@@ -6,11 +6,13 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -38,24 +40,65 @@ namespace PhotoMap.Client
 
         private void webView_WebMessageReceived(object sender, Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs e)
         {
-            //SetPushpin("59.452644095886846", "17.933108726842157");
+            var message = JsonSerializer.Deserialize<WebViewEventMessage>(e.WebMessageAsJson, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
 
-            var gps = ImageMetadataReader.ReadMetadata("pike.jpg")
-                             .OfType<GpsDirectory>()
-                             .FirstOrDefault();
-
-            var location = gps.GetGeoLocation();
-            var numberFormat = new NumberFormatInfo() { NumberDecimalSeparator = "." };
-            SetPushpin(location.Latitude.ToString(numberFormat), location.Longitude.ToString(numberFormat));
+            if (message.Event == "BingMapLoaded")
+            {
+                // ready to interact
+            }
+            else if (message.Event == "PinClicked")
+            {
+                var imagePath = System.Web.HttpUtility.UrlDecode(message.Parameter);
+                PreviewImage.Source = new BitmapImage(new Uri(imagePath));
+            }
         }
-
-        private async void SetPushpin(string latitude, string longitude)
+        private async void SetPushpin(string latitude, string longitude, string filePath)
         {
             var setPinScript = $@"
                 var pin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location({latitude}, {longitude}));
-                this.map.entities.push(pin);
+                pin.filePath = escape('{System.Web.HttpUtility.JavaScriptStringEncode(filePath)}');
+                this.map.entities.push(pin);    
+                Microsoft.Maps.Events.addHandler(pin, 'click', pinClick);
                 ";
             await webView.ExecuteScriptAsync(setPinScript);
+        }
+
+        private void ScanDirectory_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (var dialog = new FolderBrowserDialog())
+            {
+                DialogResult result = dialog.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    var folder = dialog.SelectedPath;
+                    var imageFiles = System.IO.Directory.GetFiles(folder, "*.jpg", SearchOption.AllDirectories);
+                    foreach (var imageFile in imageFiles)
+                    {
+                        try
+                        {
+                            var gps = ImageMetadataReader.ReadMetadata(imageFile)
+                                             .OfType<GpsDirectory>()
+                                             .FirstOrDefault();
+
+                            if (gps != null)
+                            {
+                                var location = gps.GetGeoLocation();
+                                if (location != null)
+                                {
+                                    var numberFormat = new NumberFormatInfo() { NumberDecimalSeparator = "." };
+                                    SetPushpin(location.Latitude.ToString(numberFormat), location.Longitude.ToString(numberFormat), imageFile);
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                    }
+
+                }
+            }
         }
     }
 }
---
+++ PhotoMap.Client/WebViewEventMessage.cs
@@ -0,0 +1,6 @@
+﻿public class WebViewEventMessage
+{
+    public string Event { get; set; }
+    public string Parameter { get; set; }
+
+}
